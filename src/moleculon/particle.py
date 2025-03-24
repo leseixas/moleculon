58e74ae092375da07b8c83021847d1d5dc0e843b
Leandro Seixas <leandro.fisica@gmail.com>
2025-03-24 18:02:28 -0300

    add argument validations

--- src/moleculon/particle.py
+++ src/moleculon/particle.py
@@ -3,26 +3,19 @@ import pandas as pd
 import scipy as sp
 
 class Particle:
-    def __init__(self, element, position, velocity, force, charge, dipole):
-        """
-        Initialize a new instance of the class.
-
-        Parameters:
-        element (str): The chemical element.
-        position (tuple): The position of the element in 3D space (x, y, z).
-        velocity (tuple): The velocity of the element in 3D space (vx, vy, vz).
-        force (tuple): The force acting on the element in 3D space (fx, fy, fz).
-        charge (float): The electric charge of the element.
-        dipole (tuple): The dipole moment of the element in 3D space (dx, dy, dz).
-        """
+    def __init__(self, element, position: np.ndarray,
+                 velocity: np.ndarray, force: np.ndarray, charge: float,
+                 electric_dipole: np.ndarray, magnetic_dipole: np.ndarray):
         self.element = element
         self.position = position
         self.velocity = velocity
         self.force = force
         self.charge = charge
-        self.dipole = dipole
+        self.electric_dipole = electric_dipole
+        self.magnetic_dipole = magnetic_dipole
 
-    def shift(self, shift):
+
+    def shift(self, shift: np.ndarray):
         """
         Shifts the position by a given value.
 
@@ -31,6 +24,7 @@ class Particle:
         """
         self.position += shift
     
+
     def rotate(self, rotation_matrix):
         """
         Rotate the position, velocity, force, and dipole of the object using the given rotation matrix.
@@ -45,6 +39,7 @@ class Particle:
         self.velocity = np.dot(rotation_matrix, self.velocity)
         self.force = np.dot(rotation_matrix, self.force)
         self.dipole = np.dot(rotation_matrix, self.dipole)
+
 
     def __str__(self):
         return f"Element: {self.element}\nPosition: {self.position}\nVelocity: {self.velocity}\nForce: {self.force}\nCharge: {self.charge}\nDipole: {self.dipole}"
